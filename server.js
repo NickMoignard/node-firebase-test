cd8a863b1cbd5f083457259c6b09d6133fcd922a
NickMoignard <nicklmoignard@gmail.com>
2016-05-26 20:46:07 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,78 @@
+/* 
+	name: server.js
+
+*/
+
+// BASE SETUP ================================================================
+
+// Call Packages
+var express = require('express'),
+	app = express(),  //define our app using express
+	bodyParser = require('body-parser'),
+	morgan = require('morgan'),
+	mongoose = require('mongoose');
+
+//set the port for our app
+var port = process.env.PORT || 8080; 
+
+// Configure App:
+// body parser, to grab information from POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+
+// configure app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POSTS');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, \
+		content-type, Authorization');
+	next();
+});
+// MIDDLEWARE ================================================================
+// log all requests to the console
+app.use(morgan('dev'));
+
+// API ROUTES ================================================================
+
+// Home page
+app.get('/', function(req, res) {
+	res.send('welcome to the home page!');
+});
+
+// get an express router
+var apiRouter = express.Router();
+
+// test route to make sure everything is working
+// accessed at GET http://localhost:8080/api
+apiRouter.get('/', function(req, res) {
+	res.json({ message: 'woah check out this json'});
+});
+
+// additonal routes will occur here
+
+// Register our routes - all routes prefixed with /api
+app.use('/api', apiRouter);
+
+//START THE SERVER ===========================================================
+app.listen(port);
+console.log('port: '+ port);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
